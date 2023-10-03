bdad0188fa728274b6fba2a78e2907c6918230cb
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-03 21:47:44 +0000

    added user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc })
-    render({ :template ->"user_templates/index"})
+    render({ :template => "user_templates/index" })
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.at(0)
+
+    # if @the_user == nil
+    #   redirect_to("/404")
+    # else
+    #   render({ :templates => "user_templates/show"})
+    # end
+    render({ :template => "user_templates/show" })
   end
 end
