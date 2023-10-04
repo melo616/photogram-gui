763288a70e66fd0a540fa153cc252b1df4348807
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-04 21:34:54 +0000

    finished user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @the_user = matching_usernames.at(0)
 
     # if @the_user == nil
-    #   redirect_to("/404")
+    #   redirect_to("/404" )
     # else
     #   render({ :templates => "user_templates/show"})
     # end
@@ -20,6 +20,9 @@ class UsersController < ApplicationController
 
   def insert_new_user
     u = User.new
+    u.username = params.fetch("username")
+
+    u.save
     
     redirect_to("/users")
   end
