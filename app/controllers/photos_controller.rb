1c826f4f120de795ad322945233d8699567e504b
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-05 15:24:42 +0000

    added delete photo functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,11 @@ class PhotosController < ApplicationController
     @the_photo = Photo.where({:id => params.fetch("path_id")}).at(0)
     render({ :template => "photo_templates/show" })
   end
+
+  def destroy
+    the_photo = Photo.where({ :id => params.fetch("path_id")}).at(0)
+    the_photo.destroy
+    redirect_to("/photos")
+  end
 end
+
