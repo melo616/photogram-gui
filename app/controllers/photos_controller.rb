5a7e2a63efcff4afdd95b8b4d15654a29f94c800
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-05 16:23:33 +0000

    create photo added

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   def index
+    @list_of_photos = Photo.all.order({ :created_at => :desc})
     render({ :template => "photo_templates/index"})
   end
 
@@ -8,10 +9,29 @@ class PhotosController < ApplicationController
     render({ :template => "photo_templates/show" })
   end
 
+  def create
+    new_photo = Photo.new
+
+    new_photo.image = params.fetch("image_url")
+    new_photo.caption = params.fetch("caption")
+    new_photo.owner_id = params.fetch("owner_id")
+
+    new_photo.save
+    redirect_to("/photos/#{new_photo.id}")
+  end
+  
+  def update
+    the_photo = Photo.where({ :id => params.fetch("path_id" )}).at(0)
+    the_photo.image = params.fetch("image_url")
+    the_photo.caption = params.fetch("caption")
+
+    the_photo.save
+    redirect_to("/photos/#{the_photo.id}")
+  end
+
   def destroy
     the_photo = Photo.where({ :id => params.fetch("path_id")}).at(0)
     the_photo.destroy
     redirect_to("/photos")
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
-
+  post("/insert_new_photo", { :controller => "photos", :action => "create" })
+  post("/update_photo/:path_id", { :controller => "photos", :action => "update" })
   get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
 end
